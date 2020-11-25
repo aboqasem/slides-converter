2954a5d951ea41b8cb6e399b5a1a5620c8c4344f
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-11-25 12:50:24 +0800

    Improve logging

--- src/optimize.js
+++ src/optimize.js
@@ -9,9 +9,11 @@ fs.readdirSync(lecturesDir).forEach(pdfFileName => {
   if (pdfFileName.includes('Lec')) {
     const dirToPdf = `${lecturesDir}/${pdfFileName}`;
     const dirToOptimizedPdf = `${lecturesDir}/${pdfFileName.replace('.pdf', '_optimized.pdf')}`;
+    console.log(`Optimizing ${pdfFileName}...`);
     shell.exec(
       `gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -dNOPAUSE -dQUIET -dBATCH -sOutputFile="${dirToOptimizedPdf}" "${dirToPdf}"
       rm "${dirToPdf}"
       mv "${dirToOptimizedPdf}" "${dirToPdf}"`);
+    console.log(`Done ${pdfFileName}.`);
   }
 });
